c2a1d95d6e6ac2564f3c38e1b6235065d6792707
Pedro Martin <pataruco@gmail.com>
2017-10-15 14:42:41 +0100

    Create a getter and setter for Mission Control Robot Position

--- src/mission-control.js
+++ src/mission-control.js
@@ -4,7 +4,12 @@ module.exports = class MissionControl {
             coordinates: {
                 x: 0,
                 y: 0
-            }
+            },
+            robotPosition: {
+                x: 0,
+                y: 0,
+                orientation: 'n'
+           }
         }
     }
 
@@ -25,4 +30,46 @@ module.exports = class MissionControl {
     get coordinates( ) {
         return this.data.coordinates;
     }
+
+    set robotPosition( string ) {
+        const pattern = /^\d{1,2}?\s\d{1,2}\s[NnEeSsWw]$/g;
+        const isARobotPositionString = pattern.test( string );
+        if ( isARobotPositionString ){
+            return this.setRobotPositionObject( string )
+        }
+        return this.data.robotPosition = false;
+    }
+
+    get robotPosition( ) {
+        return this.data.robotPosition;
+    }
+
+    setRobotPositionObject( string ){
+        this.data.robotPosition.x = parseInt( string.split(' ')[0] );
+        this.data.robotPosition.y = parseInt( string.split(' ')[1] );
+        this.data.robotPosition.orientation = string.split(' ')[2].toLowerCase( );
+        return this.checkRobotPositionOnTheGrid();
+    }
+
+    checkRobotPositionOnTheGrid( ) {
+        if ( this.data.robotPosition.x > this.data.coordinates.x ) {
+            return this.data.robotPosition = null;
+        }
+
+        if ( this.data.robotPosition.y > this.data.coordinates.y ) {
+            return this.data.robotPosition = null;
+        }
+        return this.checkRobotPositionCoordinates()
+    }
+
+    checkRobotPositionCoordinates( ) {
+        if ( this.data.robotPosition.x > 50) {
+            this.data.robotPosition.x = 50;
+        }
+
+        if ( this.data.robotPosition.y > 50) {
+            this.data.robotPosition.y = 50;
+        }
+    }
+
 }
--- test/mission-control-test.js
+++ test/mission-control-test.js
@@ -31,6 +31,54 @@ describe('MissionControl', ( ) => {
         it('should GET coordinates', ( ) => {
             expect( missionControl.coordinates ).to.include({x: 5, y:3})
         });
+        // - if not throw error message
+        it('should return false when user input does not match coordinates pattern', ( ) => {
+            const mission = new MissionControl( );
+            mission.coordinates = 'aa bb';
+            expect(mission.coordinates).to.be.false;
+        });
+    });
 
-    })
+    describe('robot position', ( ) => {
+        it('should SET robot position', ( ) => {
+            missionControl.robotPosition = '1 1 E'
+            expect( missionControl.data.robotPosition ).to.include({x: 1, y:1, orientation: 'e'})
+        });
+
+        it('should GET robot position', ( ) => {
+            missionControl.robotPosition = '2 2 N'
+            expect( missionControl.robotPosition ).to.include({x: 2, y:2, orientation: 'n'})
+        });
+
+        // The maximum value for any coordinate is 50.
+        it('should SET robot position data to be 50 if coordinates are greater than 51', ( ) => {
+            missionControl.coordinates = '70 70';
+            missionControl.robotPosition = '60 60 N'
+            expect( missionControl.robotPosition ).to.include({x: 50, y:50, orientation: 'n'})
+        });
+
+        // - check if coordinates are less than world grid dimensions
+        describe('are in the world grid?', ( ) => {
+            it('should SET coordinates if are in the world grid', ( ) => {
+                missionControl.coordinates = '5 3';
+                missionControl.robotPosition = '2 2 N'
+                expect( missionControl.robotPosition ).to.include({x: 2, y:2, orientation: 'n'})
+            });
+
+            it('should prevent SET coordinates if are beyond the world grid', ( ) => {
+                missionControl.coordinates = '5 3';
+                missionControl.robotPosition = '7 4 N'
+                expect( missionControl.robotPosition ).to.be.null;
+            });
+        })
+
+        describe('error ', ( ) => {
+            //         - check if coordinates match pattern
+            it('should return false when user input does not match coordinates and orientation pattern', ( ) => {
+                const mission = new MissionControl( );
+                mission.robotPosition = '11E'
+                expect(mission.robotPosition).to.be.false;
+            });
+        });
+    });
 });

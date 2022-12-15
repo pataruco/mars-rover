79ec1f07ac01f66ae6a857d2af356673bfaec2f9
Pedro Martin <pataruco@users.noreply.github.com>
2022-12-15 11:24:27 +0000

    Move Robot

--- typescript/robot/index.test.ts
+++ typescript/robot/index.test.ts
@@ -1,34 +1,85 @@
 import Robot from '.';
 import type { RobotParams } from '.';
 
+const robotOneParams: RobotParams = {
+  worldDimensions: {
+    x: 5,
+    y: 3,
+  },
+  instructions: ['r', 'f', 'r', 'f', 'r', 'f', 'r', 'f'],
+  initialPosition: {
+    x: 1,
+    y: 1,
+    orientation: 'e',
+  },
+};
+
+const robotTwoParams: RobotParams = {
+  worldDimensions: {
+    x: 5,
+    y: 3,
+  },
+  instructions: [
+    'f',
+    'r',
+    'r',
+    'f',
+    'l',
+    'l',
+    'f',
+    'f',
+    'r',
+    'r',
+    'f',
+    'l',
+    'l',
+  ],
+  initialPosition: {
+    x: 3,
+    y: 2,
+    orientation: 'n',
+  },
+};
+
+const robotThreeParams: RobotParams = {
+  worldDimensions: {
+    x: 5,
+    y: 3,
+  },
+  instructions: ['l', 'l', 'f', 'f', 'l', 'f', 'l', 'f', 'l'],
+  initialPosition: {
+    x: 0,
+    y: 3,
+    orientation: 'w',
+  },
+};
+
 describe('Robot', () => {
   it('accept params', () => {
-    const params: RobotParams = {
-      worldDimensions: {
-        x: 5,
-        y: 3,
-      },
-      instructions: ['r', 'f', 'r', 'f', 'r', 'f', 'r', 'f'],
-      initialPosition: {
-        x: 1,
-        y: 1,
-        orientation: 'e',
-      },
-    };
-
     const {
       worldDimensions: dimensions,
       instructions,
       initialPosition,
-    } = params;
+    } = robotOneParams;
 
-    const robot = new Robot(params);
+    const robot = new Robot(robotOneParams);
 
     expect(robot.data).toEqual({
       dimensions,
       instructions,
       previousPosition: initialPosition,
       finalPosition: initialPosition,
+    });
+  });
+
+  it('moves to a new position', () => {
+    const robot = new Robot(robotTwoParams);
+    robot.move();
+
+    expect(robot.data.finalPosition).toEqual({
+      x: 3,
+      y: 3,
+      orientation: 'n',
     });
   });
 });
--- typescript/robot/index.ts
+++ typescript/robot/index.ts
@@ -3,13 +3,17 @@ interface Coordinate {
   y: number;
 }
 
+type Orientation = 'n' | 'e' | 's' | 'w';
+
 interface Position extends Coordinate {
-  orientation: 'n' | 'e' | 's' | 'w';
+  orientation: Orientation;
 }
+
+type Instruction = 'l' | 'r' | 'f';
 
 export interface RobotParams {
   worldDimensions: Coordinate;
-  instructions: string[];
+  instructions: Instruction[];
   initialPosition: Position;
 }
 
@@ -17,7 +21,7 @@ export default class Robot {
   data: {
     previousPosition: Position;
     finalPosition: Position;
-    instructions: string[];
+    instructions: Instruction[];
     dimensions: Coordinate;
   };
 
@@ -41,17 +45,68 @@ export default class Robot {
       dimensions: worldDimensions,
     };
   }
-}
 
-const robot = new Robot({
-  worldDimensions: {
-    x: 5,
-    y: 3,
-  },
-  instructions: ['r', 'f', 'r', 'f', 'r', 'f', 'r', 'f'],
-  initialPosition: {
-    x: 1,
-    y: 1,
-    orientation: 'e',
-  },
-});
+  checkRudder(instruction: Instruction, orientation: Orientation) {
+    const rudder: Orientation[] = ['n', 'e', 's', 'w'];
+
+    let index = rudder.indexOf(orientation);
+
+    if (instruction === 'r') {
+      index = rudder.indexOf(orientation) + 1;
+    }
+
+    if (instruction === 'l') {
+      index = rudder.indexOf(orientation) - 1;
+    }
+
+    if (index > 3) {
+      return rudder[0];
+    }
+
+    if (index < 0) {
+      return rudder[3];
+    }
+
+    return rudder[index];
+  }
+
+  moveForward(orientation: Orientation) {
+    switch (orientation) {
+      case 'n':
+        this.data.finalPosition.y++;
+        break;
+      case 'e':
+        this.data.finalPosition.x++;
+        break;
+      case 's':
+        this.data.finalPosition.y--;
+        break;
+      case 'w':
+        this.data.finalPosition.x--;
+        break;
+    }
+  }
+
+  public move() {
+    for (let instruction of this.data.instructions) {
+      this.data.previousPosition = this.data.finalPosition;
+      switch (instruction) {
+        case 'r':
+          this.data.finalPosition.orientation = this.checkRudder(
+            'r',
+            this.data.finalPosition.orientation,
+          );
+          break;
+        case 'l':
+          this.data.finalPosition.orientation = this.checkRudder(
+            'l',
+            this.data.finalPosition.orientation,
+          );
+          break;
+        case 'f':
+          this.moveForward(this.data.finalPosition.orientation);
+          break;
+      }
+    }
+  }
+}

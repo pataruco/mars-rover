55cf5f10f0f77c1f4b99de1bd2ad092a6b1414a2
Pedro Martin <pataruco@users.noreply.github.com>
2023-01-31 20:05:14 +0000

    Move forward

--- python/robot/index.py
+++ python/robot/index.py
@@ -1,7 +1,6 @@
+from dataclasses import dataclass
+from typing import Dict, List
 
-from typing import Self, Dict, List
-
-from attr import dataclass
 
 @dataclass
 class Robot:
@@ -9,54 +8,56 @@ class Robot:
     instructions: List[str]
     dimensions: Dict[str, str]
 
-
-
     def move(self):
-        for instruction in self.instructions: 
-          match instruction:
-              case 'r':
-                  self.position["orientation"] = self.check_rudder('r', self.position["orientation"])
-              case 'l':
-                  self.position["orientation"] = self.check_rudder('l', self.position["orientation"])
-              case 'f':
-                  print("forward")
+        for instruction in self.instructions:
+            match instruction:
+                case "r":
+                    self.position["orientation"] = self.check_rudder("r", self.position["orientation"])
+                case "l":
+                    self.position["orientation"] = self.check_rudder("l", self.position["orientation"])
+                case "f":
+                    self.move_forward(self.position["orientation"])
 
-          print({
-            "instruction": instruction,
-            "position": self.position
-          })
-        
+            print({"instruction": instruction, "position": self.position})
 
-    def check_rudder(instruction, orientation):
-        rudder = ['n', 'e', 's', 'w']
+    def check_rudder(self, instruction, orientation):
+        rudder = ["n", "e", "s", "w"]
 
         index = rudder.index(orientation)
 
-        if instruction == 'r':
-          index = rudder.indexOf(orientation) + 1;
-        if instruction == 'l':
-          index = rudder.indexOf(orientation) - 1;
-    
+        if instruction == "r":
+            index = rudder.index(orientation) + 1
+        if instruction == "l":
+            index = rudder.index(orientation) - 1
+
         if index > 3:
-           return rudder[0]
+            return rudder[0]
         if index < 0:
-           return rudder[3]
-        
+            return rudder[3]
+
         return rudder[index]
 
-                  
-
+    def move_forward(self, orientation):
+        match orientation:
+            case "n":
+                self.position["y"] = self.position["y"] + 1
+            case "e":
+                self.position["x"] = self.position["x"] + 1
+            case "s":
+                self.position["y"] = self.position["y"] - 1
+            case "w":
+                self.position["x"] = self.position["y"] - 1
 
 
 robot = Robot(
-    instructions=['r', 'f', 'r', 'f', 'r', 'f', 'r', 'f'], 
-    dimensions='', 
+    instructions=["r", "f", "r", "f", "r", "f", "r", "f"],
+    dimensions="",
     position={
-      "x": 1,
-      "y": 1,
-      "orientation" : 'e',
-      }
-    )
+        "x": 1,
+        "y": 1,
+        "orientation": "e",
+    },
+)
 
 
 robot.move()

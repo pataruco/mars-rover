f0e941a852e6577a1387432fa173d79f77656ccb
Pedro Martin <pataruco@gmail.com>
2023-03-08 12:37:03 +0000

    Test robot two leave a lost coordinate

--- python/robot/index_test.py
+++ python/robot/index_test.py
@@ -36,16 +36,25 @@ robot_three_params = {
 
 
 def test_accept_params():
-    robot_one = Robot(instructions=robot_one_params["instructions"], position=robot_one_params["position"])
+    robot_one = Robot(
+        instructions=robot_one_params["instructions"],
+        position=robot_one_params["position"],
+    )
     assert (robot_one.instructions) == ["r", "f", "r", "f", "r", "f", "r", "f"]
 
 
 def test_move_to_a_new_position():
-    robot_one = Robot(instructions=robot_one_params["instructions"], position=robot_one_params["position"])
+    robot_one = Robot(
+        instructions=robot_one_params["instructions"],
+        position=robot_one_params["position"],
+    )
     robot_one.set_world_dimmensions(world_dimensions)
     robot_one.move()
 
-    robot_two = Robot(instructions=robot_two_params["instructions"], position=robot_two_params["position"])
+    robot_two = Robot(
+        instructions=robot_two_params["instructions"],
+        position=robot_two_params["position"],
+    )
     robot_two.set_world_dimmensions(world_dimensions)
     robot_two.move()
 
@@ -54,16 +63,48 @@ def test_move_to_a_new_position():
 
 
 def test_reports_last_known_coordinates_when_is_lost():
-    robot_one = Robot(instructions=robot_one_params["instructions"], position=robot_one_params["position"])
-    robot_one.set_world_dimmensions(world_dimensions)
-    robot_one.move()
+    robot_two_params = {
+        "instructions": [
+            "f",
+            "r",
+            "r",
+            "f",
+            "l",
+            "l",
+            "f",
+            "f",
+            "r",
+            "r",
+            "f",
+            "l",
+            "l",
+        ],
+        "position": {
+            "x": 3,
+            "y": 2,
+            "orientation": "n",
+        },
+    }
 
-    robot_two = Robot(instructions=robot_two_params["instructions"], position=robot_two_params["position"])
-    robot_two.set_world_dimmensions(world_dimensions)
-    robot_two.move()
+    robot = Robot(
+        instructions=robot_two_params["instructions"],
+        position=robot_two_params["position"],
+    )
 
-    assert robot_one.position == {"x": 1, "y": 1, "orientation": "e"}
-    assert robot_one.is_lost == False
+    robot.set_world_dimmensions(world_dimensions)
+    robot.move()
 
-    assert robot_two.position == {"x": 3, "y": 4, "orientation": "n"}
-    assert robot_two.is_lost == True
+    assert robot.position == {"x": 3, "y": 4, "orientation": "n"}
+    assert robot.is_lost == True
+    assert robot.lost_coordinate == {"x": 3, "y": 3, "orientation": "n"}
+
+
+def test_prevent_get_lost_if_robot_is_in_a_lost_coordinate():
+    robot_three_params = {
+        "instructions": ["l", "l", "f", "f", "f", "l", "f", "l", "f", "l"],
+        "position": {
+            "x": 0,
+            "y": 3,
+            "orientation": "w",
+        },
+    }
